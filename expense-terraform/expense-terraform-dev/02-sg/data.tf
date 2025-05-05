8fb19eb33dc839a3b7f44a0b660409cc5cef6260
vishnu jeeth <jeeth.pam99@gmail.com>
2025-05-05 12:50:31 +0530

    modified

--- expense-terraform/expense-terraform-dev/02-sg/data.tf
+++ expense-terraform/expense-terraform-dev/02-sg/data.tf
@@ -1,8 +1,3 @@
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project}/${var.env}/vpc_id"
 }
-
-data "aws_ssm_parameter" "bastion_sg_id" {
-  name = "/${var.project}/${var.env}/bastion_sg_id"
-  
-}
