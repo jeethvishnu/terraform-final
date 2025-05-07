5c38c1ddca60ff75aef4162930e42db270b7a1ed
vishnu jeeth <jeeth.pam99@gmail.com>
2025-05-07 10:36:04 +0530

    modified

--- expense-terraform/expense-terraform-dev/05-apps/expense.sh
+++ expense-terraform/expense-terraform-dev/05-apps/expense.sh
@@ -2,7 +2,7 @@
 #user data will get sudo access defaultly
 dnf install ansible -y
 cd /tmp
-git clone https://github.com/jeethvishnu/ansible-roles.git
+git clone https://github.com/jeethvishnu/ansible-roles
 cd expense-ansible-roles
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
