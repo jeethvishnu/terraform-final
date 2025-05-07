1ae827a89cea80cfae032868df7050d4596ffcda
vishnu jeeth <jeeth.pam99@gmail.com>
2025-05-07 11:00:26 +0530

    modified

--- expense-terraform/expense-terraform-dev/05-apps/expense.sh
+++ expense-terraform/expense-terraform-dev/05-apps/expense.sh
@@ -3,7 +3,7 @@
 dnf install ansible -y
 cd /tmp
 git clone https://github.com/jeethvishnu/ansible-roles
-cd expense-ansible-roles
+cd ansible-roles
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
 
