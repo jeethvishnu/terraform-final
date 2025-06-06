5e8ccbe9e36b62a9d5790ed031c40bb895f9f643
vishnu jeeth <jeeth.pam99@gmail.com>
2025-06-06 16:16:47 +0530

    modified files

--- expense-infra-dev/04-db/provider.tf
+++ expense-infra-dev/04-db/provider.tf
@@ -9,7 +9,7 @@ terraform {
     bucket = "vjeeth-dev1"
     key    = "infra-db"
     region = "us-east-1"
-    dynamodb_table = "dev-table"  # we will use for s3 locking file this table should be there
+    dynamodb_table = "terraform-lock"  # we will use for s3 locking file this table should be there
   }
 }
 
