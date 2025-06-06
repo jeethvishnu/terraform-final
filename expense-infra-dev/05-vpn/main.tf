92ff0a979d55330c2c9225f95ecac48a7a4e852c
vishnu jeeth <jeeth.pam99@gmail.com>
2025-06-06 17:23:05 +0530

    modified files

--- expense-infra-dev/05-vpn/main.tf
+++ expense-infra-dev/05-vpn/main.tf
@@ -1,20 +1,14 @@
-resource "aws_key_pair" "vpn" {
-    key_name = "vpn"
-    #can paste the pub key directly
-    public_key = file("~/.ssh/openvpn.pub")
-  
-}
-
 module "vpn" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  key_name = aws_key_pair.vpn.key_name
+  key_name = "vpn-pb"
   name = "${var.project}-${var.env}-vpn"
 
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
   #convert string lst to lst and get first element
   subnet_id              = local.public_subnet_id
   ami    = data.aws_ami.ami_info.id
+  associate_public_ip_address = true
   
   
 
