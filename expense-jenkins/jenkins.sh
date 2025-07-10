a25aa30ed2c9749e365b03318c207e29a235037d
vishnu jeeth <jeeth.pam99@gmail.com>
2025-07-10 21:30:38 +0530

    modified

--- expense-jenkins/jenkins.sh
+++ expense-jenkins/jenkins.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-curl -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+cat <<EOF >> /etc/yum.repos.d/Jenkins.repo
+[jenkins]
+name=Jenkins
+baseurl=http://pkg.jenkins.io/redhat
+gpgcheck=1
+EOF
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-21-openjdk jenkins -y
 systemctl daemon-reload
