58e66b083df90ee0d5926650ed1e9aefe2152b6e
vishnu jeeth <jeeth.pam99@gmail.com>
2025-07-21 18:20:37 +0530

    files

--- expense-jenkins/main.tf
+++ expense-jenkins/main.tf
@@ -34,6 +34,36 @@ module "jenkins-agent" {
   }
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  public_key = file("~/.ssh/tools.pub")
+  
+}
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-0d91541a2fc6f3893"]
+  subnet_id     = "subnet-0c31f5cfaef3e61d2"
+  ami = data.aws_ami.nexus_ami.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp2"
+      volume_size = 30
+    }
+  ]
+
+  tags = {
+    Name = "nexus"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 3.0"
@@ -58,6 +88,14 @@ module "records" {
         module.jenkins-agent.private_ip
       ]
     },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.nexus.private_ip
+      ]
+    }
   ]
 
 }
